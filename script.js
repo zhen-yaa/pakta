7d641ab5d1fbc6d02c0c26e47ee6c180a5f6ab30
zhen-yaa <nachtfaust4demons@gmail.com>
2025-03-04 18:09:30 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "Crush kopa gyapon si mamon^^";
+    gif.src=
+    "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gifyou.webp";
+});
+
+noBtn.addEventListener("mouseover", () => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerWidth - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomX + "px";
+});
